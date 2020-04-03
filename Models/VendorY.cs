7a551035f80719aaa2d6753b0e736b69b76356cc
yusuf-iwpi <yusuf.saymeanst@gmail.com>
2020-04-03 15:12:35 +0700

    vendors yusuf

---
+++ Models/VendorY.cs
@@ -0,0 +1,9 @@
+namespace  Models
+{
+    public class vendor
+    {
+        public string No {get; set;}
+        public string Name {get; set;}
+        public string Phone_No {get; set;}
+    }
+}
